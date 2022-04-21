e9b6121628f9f15931d0c14b64118c137ebad52e
Kaio_Fabiano <kaio.fabiano@outlook.com>
2022-04-21 10:35:02 -0300

    feat: add product component

--- frontend/src/app/modules/main/components/product/product.component.ts
+++ frontend/src/app/modules/main/components/product/product.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AxiosError } from 'axios';
+import Swal from 'sweetalert2';
+import { Product } from './models/product.model';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +11,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  products?: Array<Product>;
+  page: number = 1;
+  contador: number = 5;
+  init: boolean = true;
+  max: boolean = false;
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {
+    this.pagenate(0);
+  }
+
+  public async createProduct() {
+    this.router.navigateByUrl('produtcs/create');
+  }
+
+  public async updateProduct(id: number) {
+    this.router.navigateByUrl('produtcs/update/' + id);
+  }
+
+  public async deleteProduct(id: number) {
+    try {
+      Swal.fire({
+        title: 'Tem certeza?',
+        text: 'Essa operação não pode ser revertida!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, deletar!',
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          const newproduct = await this.productService.deleteProduct(id);
+          this.products?.splice(
+            this.products.findIndex((product) => product.id === id),
+            1
+          );
+          Swal.fire('Deletado!', 'O produto foi deletado', 'success');
+        }
+      });
+    } catch (error) {
+      const data = error as AxiosError;
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: data.response?.data,
+      });
+    }
+  }
+
+  public goToClients() {
+    this.router.navigateByUrl('clients');
+  }
+
+  public goToDemands() {
+    this.router.navigateByUrl('demands');
+  }
+
+  public async pagenate(direction: number) {
+    this.page += direction;
+    const isClient = await this.productService
+      .listPageProducts(this.page + 1, 10)
+      .then((isClient) => {
+        isClient.length == 0 ? (this.max = true) : (this.max = false);
+      });
+    if (this.page == 1) {
+      this.init = true;
+    } else {
+      this.init = false;
+    }
+    this.products = await this.productService.listPageProducts(this.page, 10);
   }
 
 }
